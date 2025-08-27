d68e10c442951633fbf483be22949391edd177ba
nezam <nezam1999d@gmail.com>
2025-08-27 12:31:46 -0500

    fix issues language middleware

--- core/settings.py
+++ core/settings.py
@@ -48,8 +48,9 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+        "django.middleware.locale.LocaleMiddleware",
     "core.middleware.APILanguageMiddleware",
-    "django.middleware.locale.LocaleMiddleware",
+
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
