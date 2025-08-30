dd0ff9f8cd519d3f1bd1bde48d951fe863357e71
nezam <nezam1999d@gmail.com>
2025-08-30 06:09:21 -0500

    update account views

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,6 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
-from django.contrib.auth import authenticate
 from rest_framework.decorators import api_view, permission_classes
 
 from .models import CustomUser, Profile
@@ -13,7 +12,6 @@ from .serilizer import (
     RegisterSerializer, UserSerializer, UserUpdateSerializer,
     ChangePasswordSerializer, ProfileSerializer
 )
-
 
 class RegisterView(APIView):
     """User registration endpoint that returns JWT tokens"""
