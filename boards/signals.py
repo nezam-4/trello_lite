b4ff5b039e383b799dfbe4dd883d576a36a1db03
nezam <nezam1999d@gmail.com>
2025-09-05 13:14:01 -0500

    add signal for creat default list in board

--- boards/signals.py
+++ boards/signals.py
@@ -1,4 +1,24 @@
+from django.db import transaction
+from django.dispatch import receiver
 from django.db.models.signals import post_save
+from boards.models import Board
+from lists.models import List
+
+
+@receiver(post_save, sender=Board)
+def create_default_lists(sender, instance, created, **kwargs):
+    """
+    Automatically create three default lists ('Todo', 'Doing', 'Done') when a new board is created.
+    """
+    if created:
+        default_titles = ['Todo', 'Doing', 'Done']
+        with transaction.atomic():
+            for index, title in enumerate(default_titles, start=1):
+                List.objects.create(
+                    board=instance,
+                    title=title,
+                    position=index
+                )
 
 
 
