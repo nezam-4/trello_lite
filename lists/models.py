d052c907836be4ba52350cca45e6bec4aa7c5359
nezam <nezam1999d@gmail.com>
2025-09-08 18:29:40 -0500

    add feature move_to_position to list model

--- lists/models.py
+++ lists/models.py
@@ -24,27 +24,44 @@ class List(models.Model):
         super().save(*args, **kwargs)
     
     def move_to_position(self, new_position):
-        """Move list to a new position"""
+        """Move list to a new position using high temporary positions to avoid constraint violations"""
         old_position = self.position
         if old_position == new_position:
             return
         
-        # Shift other lists
-        if new_position > old_position:
-            List.objects.filter(
-                board=self.board,
-                position__gt=old_position,
-                position__lte=new_position
-            ).update(position=models.F('position') - 1)
-        else:
-            List.objects.filter(
-                board=self.board,
-                position__gte=new_position,
-                position__lt=old_position
-            ).update(position=models.F('position') + 1)
+        # Use a transaction to ensure atomicity
+        from django.db import transaction
         
-        self.position = new_position
-        self.save(update_fields=['position'])
+        with transaction.atomic():
+            # Find a safe temporary position (higher than any existing position)
+            max_position = List.objects.filter(board=self.board).aggregate(
+                models.Max('position')
+            )['position__max'] or 0
+            temp_position = max_position + 1000  # Use a high temporary position
+            
+            # Step 1: Move this list to temporary position to avoid conflicts
+            self.position = temp_position
+            self.save(update_fields=['position'])
+            
+            # Step 2: Shift other lists
+            if new_position > old_position:
+                # Moving down: shift lists up (decrease their positions)
+                List.objects.filter(
+                    board=self.board,
+                    position__gt=old_position,
+                    position__lte=new_position
+                ).update(position=models.F('position') - 1)
+            else:
+                # Moving up: shift lists down (increase their positions)
+                List.objects.filter(
+                    board=self.board,
+                    position__gte=new_position,
+                    position__lt=old_position
+                ).update(position=models.F('position') + 1)
+            
+            # Step 3: Move this list to its final position
+            self.position = new_position
+            self.save(update_fields=['position'])
     
     @property
     def tasks_count(self):
